97fe32495a61532546807988b4cd4bfbd1007a06
Mike Bionic <muhammed@millione.tm>
2023-06-06 21:05:47 +0500

    updated migration json data

--- migrate.py
+++ migrate.py
@@ -42,117 +42,30 @@ try:
 		db.session.add(current_user)
 		db.session.commit()
 
-
-	persons_data = [
-		{
-			"note": "",
-			"date": datetime(2020,11,23),
-			"name": "Süleýman",
-			"surname": "Ataýew",
-			"patronomic": "Ahmedowiç",
-			"home_address": "Balkan w. Türkmenbaşy ş. Andalyp",
-			"m_place": "000 h/b Balkan wel",
-			"m_job": "",
-			"m_level": "Okuwçy",
-			"birth_date": datetime(2002,10,14),
-			"birth_place": "",
-			"country": "",
-			"state": "",
-			"city": "",
-			"address": "",
-			"ehw_name": "Türkmenbaşy ş. hw",
-			"knowledge_level": "Orta",
-			"major": "",
-			"last_job": "Öý işler",
-			"m_call_year": 2020,
-			"sport_level": "",
-		},
-		{
-			"note": "",
-			"date": datetime(2020,11,23),
-			"name": "Welmyrat",
-			"surname": "Weliyew",
-			"patronomic": "Ahmedowiç",
-			"home_address": "Balkan w. Türkmenbaşy ş. Andalyp",
-			"m_place": "000 h/b Balkan wel",
-			"m_job": "",
-			"m_level": "Talyp",
-			"birth_date": datetime(2002,10,14),
-			"birth_place": "",
-			"country": "",
-			"state": "",
-			"city": "",
-			"address": "",
-			"ehw_name": "Türkmenbaşy ş. hw",
-			"knowledge_level": "Orta",
-			"major": "",
-			"last_job": "Öý işler",
-			"m_call_year": 2020,
-			"sport_level": "",
-		},
-	]
-
-
-	physicals_data = [
-		{
-			"note": "",
-			"person_id": None,
-			"first_review_date": datetime(2020,11,22),
-			"height": 181,
-			"weight": 62,
-			"lungs_default": 86,
-			"lungs_inhaled": 88,
-			"lungs_exhaled": 85,
-			"spirometry": "",
-			"dinamometry_right": "",
-			"dinamometry_left": "",
-		}
-	]
-
-	mcheckups_data = [
-		{
-			"note": "",
-			"person_id": None,
-			"first_review_date": datetime(2020,11,22),
-			"allergy": "Dermana, iýmite allergiýasy ýok",
-			"has_allergy": 0,
-			"body_construction": "Normosteniki",
-			"body_state": "Adaty",
-			"bone_muscle": "Göze görünýän deformasiýa ýok",
-			"breathing": "Öýkenlerde wezikulýar dem alyş eşdilýär. DAÝ:16'",
-			"blood_cycling": "Ýürek toulary aýdyň, ritmiki",
-			"pulse": "P8: 78'",
-			"blood_pressure": "AGB: 170/80 mmss",
-			"blood_category": "4B+",
-			"stomach": "Dili çygly, arassa. Garny ýumşak, agry ýok. Bagry we dalagy ",
-			"vision": "OU = 1.0",
-			"withGlass": 0,
-			"hearing": "Pyşdyly sesi 6m eşidýär",
-			"nerve": "Durnukly",
-			"other": "K/K: Sagdyn",
-			"reviewed_medic": "",
-		}
-	]
-
-
 	p_count = 0
-	for person in persons_data:
-		current_person = Person(**person)
+	for person in migration_data["persons_data"]:
+		person["date"] = datetime.strptime(person["date"], "%d.%m.%Y") if len(person["date"]) > 5 else None
+		person["birth_date"] = datetime.strptime(person["birth_date"], "%d.%m.%Y") if len(person["birth_date"]) > 5 else None
+		current_person = Person(
+			**person,
+		)
 		db.session.add(current_person)
 		db.session.commit()
 
-		if p_count < len(physicals_data):
-			physicals_data[p_count]["person_id"] = current_person.id ##p_count ##
-			mcheckups_data[p_count]["person_id"] = current_person.id ##p_count ##
+		if p_count < len(migration_data["physicals_data"]):
+			migration_data["physicals_data"][p_count]["person_id"] = current_person.id ##p_count ##
+			migration_data["mcheckups_data"][p_count]["person_id"] = current_person.id ##p_count ##
 		p_count += 1
 
 
-	for physical in physicals_data:
+	for physical in migration_data["physicals_data"]:
+		physical["first_review_date"] = datetime.strptime(physical["first_review_date"], "%d.%m.%Y") if len(physical["first_review_date"]) > 5 else None
 		current_physical = Physical(**physical)
 		db.session.add(current_physical)
 		db.session.commit()
 
-	for mcheckup in mcheckups_data:
+	for mcheckup in migration_data["mcheckups_data"]:
+		mcheckup["first_review_date"] = datetime.strptime(mcheckup["first_review_date"], "%d.%m.%Y") if len(mcheckup["first_review_date"]) > 5 else None
 		current_mcheckup = Medical_checkup(**mcheckup)
 		db.session.add(current_mcheckup)
 		db.session.commit()
@@ -169,59 +82,18 @@ try:
 
 	############
 
-	hospitals_data = [
-		{
-			"name": "Lukmançylyk nokady",
-			"location": "",
-			"address": "Jaňga",
-			"color_code": "#FF9E27",
-			"maxval": 15,
-			"note": "",
-		},
-		{
-			"name": "Türkmenbaşy hassahana",
-			"location": "",
-			"address": "Türkmenbaşy",
-			"color_code": "#F25961",
-			"maxval": 40,
-			"note": "",
-		},
-		{
-			"name": "Aşgabat hassahana",
-			"location": "",
-			"address": "Aşgabat",
-			"color_code": "#4485c5",
-			"maxval": 40,
-			"note": "",
-		}
-	]
 
-	for hospital in hospitals_data:
+	for hospital in migration_data["hospitals_data"]:
 		current_hospital = Hospital(**hospital)
 		db.session.add(current_hospital)
 		db.session.commit()
 
 	############################
 
-	curingrs_data = [
-		{
-			"note": "",
-			"person_id": 1,
-			"hospital_id": 3,
-			"enter_date": datetime(2023,3,11),
-			"reason": "Dyrnak kesdirildi",
-		},
-		{
-			"note": "",
-			"person_id": 2,
-			"hospital_id": 1,
-			"enter_date": datetime(2023,3,8),
-			"exit_date": datetime(2023,5,10),
-			"reason": "Gyzdyrma",
-		},
-	]
 
-	for curingr in curingrs_data:
+	for curingr in migration_data["curingrs_data"]:
+		curingr["enter_date"] = datetime.strptime(curingr["enter_date"], "%d.%m.%Y") if len(curingr["enter_date"]) > 5 else None
+		curingr["exit_date"] = datetime.strptime(curingr["exit_date"], "%d.%m.%Y") if len(curingr["exit_date"]) > 5 else None
 		current_curingr = Curing_record(**curingr)
 		db.session.add(current_curingr)
 		db.session.commit()
